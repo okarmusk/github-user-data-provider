fa9166627cc1bc12d040f42731b83594e2a59280
Konrad Okarmus <okarmusk@outlook.com>
2024-07-08 22:45:13 +0200

    Cleanup (#2)

--- src/main/java/com/byteoffset/okarmusk/user/data/provider/domain/user/calculations/CalculationsException.java
+++
@@ -1,7 +0,0 @@
-package com.byteoffset.okarmusk.user.data.provider.domain.user.calculations;
-
-public class CalculationsException extends Exception {
-    public CalculationsException(String message) {
-        super(message);
-    }
-}
