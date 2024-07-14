df3490f1edc593ab48362625660d490dc1d96366
Konrad Okarmus <okarmusk@outlook.com>
2024-07-14 19:57:37 +0200

    Circuit breaker (#3)

--- src/main/java/com/byteoffset/okarmusk/user/data/provider/infrastructure/github/GithubApiClientConfiguration.java
+++ src/main/java/com/byteoffset/okarmusk/user/data/provider/infrastructure/github/GithubApiClientConfiguration.java
@@ -1,8 +1,12 @@
 package com.byteoffset.okarmusk.user.data.provider.infrastructure.github;
 
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import feign.Feign;
+import feign.Retryer;
 import feign.jackson.JacksonDecoder;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.feign.FeignDecorators;
+import io.github.resilience4j.feign.Resilience4jFeign;
+import io.github.resilience4j.ratelimiter.RateLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +16,22 @@ import java.util.List;
 @RequiredArgsConstructor
 @Configuration
 public class GithubApiClientConfiguration {
+    private final static int MAX_ATTEMPTS = 4;
     private final GithubApiConfiguration githubApiConfiguration;
 
     @Bean
     public GithubApiClient githubApiClient() {
-        return Feign.builder()
+        CircuitBreaker circuitBreaker = CircuitBreaker.ofDefaults("GithubApiClient");
+        RateLimiter rateLimiter = RateLimiter.ofDefaults("GithubApiClient");
+        FeignDecorators decorators = FeignDecorators.builder()
+                .withRateLimiter(rateLimiter)
+                .withCircuitBreaker(circuitBreaker)
+                .build();
+
+        return Resilience4jFeign.builder(decorators)
                 .decoder(new JacksonDecoder(List.of(new JavaTimeModule())))
                 .errorDecoder(new GithubErrorDecoder())
+                .retryer(new Retryer.Default(1000, 3000, MAX_ATTEMPTS))
                 .target(GithubApiClient.class, githubApiConfiguration.getUrl());
     }
 }
